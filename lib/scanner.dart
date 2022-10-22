10a2a331338e60f25a9a61198ca6ec4606e8ad13
rishuriya <rdrishav2002@yahoo.com>
2022-10-22 16:10:03 +0530

    added qr scanner

---
+++ lib/scanner.dart
@@ -0,0 +1,115 @@
+import 'dart:async';
+import 'dart:io' show Platform;
+
+import 'package:barcode_scan2/barcode_scan2.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+class Scanner extends StatefulWidget {
+  const Scanner({Key? key}) : super(key: key);
+
+  @override
+  State<Scanner> createState() => _ScannerState();
+}
+class _ScannerState extends State<Scanner> {
+  ScanResult? scanResult;
+
+  final _flashOnController = TextEditingController(text: 'Flash on');
+  final _flashOffController = TextEditingController(text: 'Flash off');
+  final _cancelController = TextEditingController(text: 'Cancel');
+
+  final _aspectTolerance = 0.00;
+  var _numberOfCameras = 0;
+  final _selectedCamera = -1;
+  final _useAutoFocus = true;
+  final _autoEnableFlash = false;
+
+  static final _possibleFormats = BarcodeFormat.values.toList()
+    ..removeWhere((e) => e == BarcodeFormat.unknown);
+
+  List<BarcodeFormat> selectedFormats = [..._possibleFormats];
+
+  @override
+  void initState() {
+    super.initState();
+
+    Future.delayed(Duration.zero, () async {
+      _numberOfCameras = await BarcodeScanner.numberOfCameras;
+      setState(() {});
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final scanResult = this.scanResult;
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Hacktober Attendence'),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.camera),
+              tooltip: 'Scan',
+              onPressed: _scan,
+            )
+          ],
+        ),
+        body: ListView(
+          scrollDirection: Axis.vertical,
+          shrinkWrap: true,
+          children: <Widget>[
+            if (scanResult != null)
+              Card(
+                child: Column(
+                  children: <Widget>[
+                    ListTile(
+                      title: const Text('Result Type'),
+                      subtitle: Text(scanResult.type.toString()),
+                    ),
+                    ListTile(
+                      title: const Text('Raw Content'),
+                      subtitle: Text(scanResult.rawContent),
+                    ),
+                    ListTile(
+                      title: const Text('Format'),
+                      subtitle: Text(scanResult.format.toString()),
+                    ),
+                    ListTile(
+                      title: const Text('Format note'),
+                      subtitle: Text(scanResult.formatNote),
+                    ),
+                  ],
+                ),
+              )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Future<void> _scan() async {
+    try {
+      final result = await BarcodeScanner.scan(
+        options: ScanOptions(
+          restrictFormat: selectedFormats,
+          useCamera: _selectedCamera,
+          autoEnableFlash:_autoEnableFlash,
+          android:AndroidOptions(
+            aspectTolerance:_aspectTolerance,
+            useAutoFocus: _useAutoFocus,
+          ),
+        ),
+      );
+      setState(() => scanResult = result);
+    } on PlatformException catch (e) {
+      setState(() {
+        scanResult = ScanResult(
+          type: ResultType.Error,
+          format: BarcodeFormat.unknown,
+          rawContent: e.code == BarcodeScanner.cameraAccessDenied
+              ? 'The user did not grant the camera permission!'
+              : 'Unknown error: $e',
+        );
+      });
+    }
+  }
+}
